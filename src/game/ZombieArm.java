acfc2c030df4c9e0ffee7d44ab15555e568d9f21
Tan Song Shun <stan0101@student.monash.edu>
2020-05-13 22:18:20 +0800

    Modified the constructor and added CraftWeaponAction as allowable action

--- src/game/ZombieArm.java
+++ src/game/ZombieArm.java
@@ -5,6 +5,7 @@ import edu.monash.fit2099.engine.WeaponItem;
 public class ZombieArm extends WeaponItem{
 	
 	public ZombieArm() {
-		super("ZombieArm",'?',25,"hits");
+		super("zombie arm",'A',20,"hits");
+		super.allowableActions.add(new CraftWeaponAction(this));
 	}
 }
