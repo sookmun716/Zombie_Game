94513e8124284e33665ea0d592213f19f0c87d59
Tan Song Shun <stan0101@student.monash.edu>
2020-05-13 22:19:14 +0800

    Created ZombieClub class to represent zombie club

---
+++ src/game/ZombieClub.java
@@ -0,0 +1,9 @@
+package game;
+
+import edu.monash.fit2099.engine.WeaponItem;
+
+public class ZombieClub extends WeaponItem {
+	public ZombieClub() {
+		super("Zombie Club",'C',30, "smacks" );
+	}
+}
