43fd2eeb78ee664e5846de9fd12cc6c26ac4aec7
Tan Song Shun <stan0101@student.monash.edu>
2020-05-13 22:20:21 +0800

    Created new class to handle crafting of weapons

---
+++ src/game/CraftWeaponAction.java
@@ -0,0 +1,45 @@
+package game;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.WeaponItem;
+
+public class CraftWeaponAction extends Action{
+	private WeaponItem weapon;
+	
+	public CraftWeaponAction(WeaponItem weapon) {
+		this.weapon = weapon;
+	}
+
+	@Override
+	public String execute(Actor actor, GameMap map) {
+		// TODO Auto-generated method stub
+		
+		if(weapon instanceof ZombieArm) {
+			actor.addItemToInventory(new ZombieClub());
+			actor.removeItemFromInventory(weapon);
+			return actor+"crafted a zombie club";
+			}
+		else if(weapon instanceof ZombieLeg) {
+			actor.addItemToInventory(new ZombieMace());
+			actor.removeItemFromInventory(weapon);
+			return actor+"crafted a zombie mace";
+		}
+		else {
+			return null;
+		}
+	}
+
+	@Override
+	public String menuDescription(Actor actor) {
+		// TODO Auto-generated method stub
+		if(weapon instanceof ZombieArm) {
+			return actor+" crafts zombie club";
+		}
+		else if(weapon instanceof ZombieLeg) {
+			return actor+" crafts zombie mace";
+		}
+		return null;
+	}
+}
