98bbb78cd2ec655313f580f0b8025dc5f6b189a3
Tan Song Shun <stan0101@student.monash.edu>
2020-05-13 22:19:34 +0800

    Created new ZombieMace class to represent zombie mace

---
+++ src/game/ZombieMace.java
@@ -0,0 +1,9 @@
+package game;
+
+import edu.monash.fit2099.engine.WeaponItem;
+
+public class ZombieMace extends WeaponItem {
+	public ZombieMace() {
+		super("Zombie Mace",'M',35, "smashes" );
+	}
+}
