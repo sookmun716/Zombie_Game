e1dc2e7986c74e09a8c97861f01e007dfe2fbdda
Hew Sook Mun <shew0010@student.monash.edu>
2020-05-13 21:27:30 +0800

    Created class ZombieLeg

---
+++ src/game/ZombieLeg.java
@@ -0,0 +1,8 @@
+package game;
+
+public class ZombieLeg extends WeaponItems{
+	
+	public ZombieLeg(String name, char displayChar, int damage, String verb) {
+		super(name, displayChar, damage, verb);
+	}
+}
