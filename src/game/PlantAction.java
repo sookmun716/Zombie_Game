50af0e120783d66e7309e47051a8c34455111a42
Tan Song Shun <stan0101@student.monash.edu>
2020-05-14 11:17:03 +0800

    Added location instance variable

--- src/game/PlantAction.java
+++ src/game/PlantAction.java
@@ -3,12 +3,18 @@ package game;
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Location;
 
 public class PlantAction extends Action{
+	
+	private Location location;
 
+	public PlantAction(Location location) {
+		this.location=location;
+	}
 	@Override
 	public String execute(Actor actor, GameMap map) {
-		map.locationOf(actor).setGround(new Crop());
+		map.at(location.x(), location.y()).setGround(new Crop());
 		return actor +" planted a crop in the dirt";
 	}
 
