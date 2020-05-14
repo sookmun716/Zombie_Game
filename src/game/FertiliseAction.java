cb54f1a2d0dbfe1f33c0e1626d94f350a9e5ac61
Tan Song Shun <stan0101@student.monash.edu>
2020-05-14 12:49:03 +0800

    Created FertiliseAction to fertilise a crop

---
+++ src/game/FertiliseAction.java
@@ -0,0 +1,28 @@
+package game;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+
+public class FertiliseAction extends Action {
+	
+	private Crop crop;
+
+	public FertiliseAction(Crop crop){
+		this.crop=crop;
+	}
+	@Override
+	public String execute(Actor actor, GameMap map) {
+		crop.setRipeTurns();
+		crop.setFertilised();
+		map.locationOf(actor).setGround(crop);
+		return actor+" fertilised crop";
+	}
+
+	@Override
+	public String menuDescription(Actor actor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
