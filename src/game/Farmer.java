31c0063caee9f6501a0787225df40654046ab4b8
Tan Song Shun <stan0101@student.monash.edu>
2020-05-14 19:25:22 +0800

    Shifted position of the three farmer behaviours

--- src/game/Farmer.java
+++ src/game/Farmer.java
@@ -4,9 +4,9 @@ public class Farmer extends Human{
 
 	protected Farmer(String name) {
 		super(name, 'F',50);
-		this.behaviour.add(0,new PlantBehaviour());
-		this.behaviour.add(1,new FertiliseBehaviour());
-		this.behaviour.add(2,new HarvestBehaviour());
+		this.behaviour.add(1,new PlantBehaviour());
+		this.behaviour.add(2,new FertiliseBehaviour());
+		this.behaviour.add(3,new HarvestBehaviour());
 	}
 	
 	
