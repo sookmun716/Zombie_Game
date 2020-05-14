12ced97a2c524f7870d860582f03b984b2eae7d9
Tan Song Shun <stan0101@student.monash.edu>
2020-05-14 18:41:04 +0800

    Added method availableCrops to check if there are ripe crops around player

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.util.ArrayList;
+
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actions;
 import edu.monash.fit2099.engine.Display;
@@ -23,12 +25,32 @@ public class Player extends Human {
 	public Player(String name, char displayChar, int hitPoints) {
 		super(name, displayChar, hitPoints);
 	}
-
+	
+	public Actions availableHarvest(Actions actions, GameMap map) {
+		ArrayList<Action> availableCrops= new ArrayList<Action>();
+		//check location adjacent to player for ripe crops
+		availableCrops=new HarvestBehaviour().checkCrop(this, map, availableCrops);
+		if(availableCrops.size()!=0) {
+			for(Action harvest : availableCrops) {
+				actions.add(harvest);
+			}
+		}
+		//check player location for ripe crop
+		if(map.locationOf(this).getGround() instanceof Crop) {
+			Crop crop=(Crop)map.locationOf(this).getGround();
+			if(crop.getripeTurns()>=20) {
+				actions.add(new HarvestAction(map.locationOf(this))) ;
+			}
+		}
+		return actions;
+	}
 	@Override
 	public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display) {
+		actions=availableHarvest(actions,map);
 		// Handle multi-turn Actions
 		if (lastAction.getNextAction() != null)
 			return lastAction.getNextAction();
 		return menu.showMenu(this, actions, display);
 	}
+	
 }
