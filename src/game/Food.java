6b43c69a4b74d9a4bbb1e159bfead3c9e4efffe7
Tan Song Shun <stan0101@student.monash.edu>
2020-05-15 13:11:46 +0800

    Removed mutator as it is not required in EatAction

--- src/game/Food.java
+++ src/game/Food.java
@@ -1,15 +1,11 @@
 package game;
 
 public class Food extends PortableItem{
-	private int healAmount=25;
 	public Food() {
 		super("Food ", 'f');
 		this.allowableActions.add(new EatAction(this));
 		// TODO Auto-generated constructor stub
 	}
 	
-	public int healAmount() {
-		return healAmount;
-	}
 
 }
