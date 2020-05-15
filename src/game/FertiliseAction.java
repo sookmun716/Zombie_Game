59798c5d33950137a051e0a6ee392522fae35bf0
Tan Song Shun <stan0101@student.monash.edu>
2020-05-15 13:07:15 +0800

    Modified execute method of FertiliseAction and removed constructor

--- src/game/FertiliseAction.java
+++ src/game/FertiliseAction.java
@@ -3,19 +3,20 @@ package game;
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Ground;
 
 public class FertiliseAction extends Action {
-	
-	private Crop crop;
-
-	public FertiliseAction(Crop crop){
-		this.crop=crop;
-	}
 	@Override
 	public String execute(Actor actor, GameMap map) {
-		crop.fertilise();
-		map.locationOf(actor).setGround(crop);
-		return actor+" fertilised crop";
+		//fertilising crops
+		for(int i=0;i<10;i++) {
+			map.locationOf(actor).getGround().tick(map.locationOf(actor));
+		}
+		//change display character to represent a fertilised crop
+		map.locationOf(actor).getGround().setDisplayChar('|');
+		return actor+" fertilised crop at "+map.locationOf(actor).x()+
+				map.locationOf(actor).y();
+		
 	}
 
 	@Override
