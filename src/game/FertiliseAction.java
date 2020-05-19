8df4d60caf80340ac76dad659969736363fd2a59
Tan Song Shun <stan0101@student.monash.edu>
2020-05-19 16:33:54 +0800

    Changed execute method to call fertilise method instead of tick

--- src/game/FertiliseAction.java
+++ src/game/FertiliseAction.java
@@ -4,16 +4,28 @@ import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Ground;
-
+/**
+ * A subclass of Action that represents an action to fertilise a crop.
+ * 
+ * @author Tan Song Shun
+ *
+ */
 public class FertiliseAction extends Action {
+	/**
+	 * This method overrides the execute method of Action, and is called to perform
+	 * an action, in this case it is used to fertilise a crop
+	 * 
+	 * @param actor The actor that is fertilising the crop.
+	 * 
+	 * @param map   The game map the actor is in.
+	 * 
+	 * @return a String that shows a crop had been fertilised.
+	 **/
 	@Override
 	public String execute(Actor actor, GameMap map) {
-		//fertilising crops
-		for(int i=0;i<10;i++) {
-			map.locationOf(actor).getGround().tick(map.locationOf(actor));
-		}
 		//change display character to represent a fertilised crop
-		map.locationOf(actor).getGround().setDisplayChar('|');
+		//fertilising crops by calling tick to increment ripeTurns
+		map.locationOf(actor).getGround().fertilise();
 		return actor+" fertilised crop at "+map.locationOf(actor).x()+
 				", "+map.locationOf(actor).y();
 		
