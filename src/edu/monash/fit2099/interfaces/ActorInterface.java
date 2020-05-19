d6210d51af09e246d9377c23aa225c5093c29f70
Tan Song Shun <stan0101@student.monash.edu>
2020-05-19 16:50:26 +0800

    Added javadoc comments for the new comments

--- src/edu/monash/fit2099/interfaces/ActorInterface.java
+++ src/edu/monash/fit2099/interfaces/ActorInterface.java
@@ -6,7 +6,15 @@ package edu.monash.fit2099.interfaces;
  */
 
 public interface ActorInterface {
+	/**
+	 * Accessor of hitPoints 
+	 * @return integer that represents the actor's current hit points
+	 */
 	public int getHp();
+	/**
+	 * Accessor of maxHitPoints 
+	 * @return integer that represents the actor's maximum hit points
+	 */
 	public int getMaxHp();
 	public void dropArm(int Arm);
 	public void dropLeg(int Leg);
