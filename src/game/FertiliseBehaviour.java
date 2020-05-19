ff5a0626dde63d56b711d299c821182f374db27d
Tan Song Shun <stan0101@student.monash.edu>
2020-05-19 16:31:45 +0800

    Added exception handling and changed the condition checking

--- src/game/FertiliseBehaviour.java
+++ src/game/FertiliseBehaviour.java
@@ -4,16 +4,29 @@ import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 
+/**
+ * A subclass of Behaviour that represents the fertilising of crops, will return
+ * a new FertiliseAction if a farmer is standing on an unfertilised and unripe
+ * crop.
+ * 
+ * @author Tan Song Shun
+ *
+ */
 public class FertiliseBehaviour implements Behaviour {
 
 	@Override
 	public Action getAction(Actor actor, GameMap map) {
-		//Farmer can only fertilise unfertilised and unripe crops.
-		if(map.locationOf(actor).getGround().getDisplayChar()!='^') {
-			return null;
+		// Farmer can only fertilise unfertilised and unripe crops.
+		try {
+			if (!map.locationOf(actor).getGround().isFertilised()) {
+				return new FertiliseAction();
+			}
 		}
-		
-		return new FertiliseAction();
+		catch(NullPointerException e) {
+			
+		}
+
+		return null;
 	}
 
 }
