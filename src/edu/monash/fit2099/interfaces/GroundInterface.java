f4a7b9c7a622f4d71fff301529b37301e1fc2e7c
Tan Song Shun <stan0101@student.monash.edu>
2020-05-19 16:50:14 +0800

    Added javadoc comments for the new methods

--- src/edu/monash/fit2099/interfaces/GroundInterface.java
+++ src/edu/monash/fit2099/interfaces/GroundInterface.java
@@ -6,8 +6,23 @@ package edu.monash.fit2099.interfaces;
  */
 
 public interface GroundInterface {
+	/**
+	 * Method that is used to determine whether a Ground Object can be used to plant crops
+	 * @return true if the ground can be used to plant crops and false otherwise
+	 */
 	public Boolean canPlant();
+	/**
+	 * Method that is used to determine whether a Crop can be fertilised
+	 * @return true if the crop can be fertilised and false otherwise, if the Ground Object is not a Crop it will return null.
+	 */
 	public Boolean isFertilised();
+	/**
+	 * Method used to fertilise a crop, the ripeTurns will be incremented by 10.
+	 */
 	public void fertilise();
+	/**
+	 *Method that is used to determine whether a Crop is ripe
+	 * @return true if the crop is ripe and false otherwise, if the Ground Object is not a Crop it will return null.
+	 */
 	public Boolean isRipe();
 }
