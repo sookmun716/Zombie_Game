7d271601c990146059de096b2da1da52011f41ce
Tan Song Shun <stan0101@student.monash.edu>
2020-05-19 17:42:09 +0800

    Changed parent class of Corpse from Item to PortableItem and override tick method so that a corpse will reanimate even if its in actor inventory

--- src/game/Corpse.java
+++ src/game/Corpse.java
@@ -1,8 +1,10 @@
 package game;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.Exit;
 import edu.monash.fit2099.engine.Item;
 import edu.monash.fit2099.engine.Location;
 
@@ -12,10 +14,10 @@ import edu.monash.fit2099.engine.Location;
  * @author Tan Song Shun
  *
  */
-public class Corpse extends Item {
+public class Corpse extends PortableItem {
 	protected Random random = new Random();
 	private String actor_name;
-	private Actor actor;
+	private Actor deadActor;
 	private int deadTurns = 0;
 
 	/**
@@ -24,15 +26,15 @@ public class Corpse extends Item {
 	 * @param actor Represent the actor that is killed
 	 */
 	public Corpse(Actor actor) {
-		super("dead " + actor, '%', false);
-		this.actor = actor;
+		super("dead " + actor, '%');
+		this.deadActor = actor;
 		actor_name = this.name.split(" ")[1];
 	}
 
 	/**
-	 * This method overrides the tick method from Item class and is called in every
+	 * This method overrides the tick method from PortableItem class and is called in every
 	 * turn, in this case it tracks the turns a Corpse is created and reanimate the
-	 * corpse if the conditions are met
+	 * corpse if the conditions are met.
 	 * 
 	 * @param currentLocation Location of the Corpse.
 	 * 
@@ -41,24 +43,72 @@ public class Corpse extends Item {
 	@Override
 	public void tick(Location currentLocation) {
 		// if actor is human proceed with resurrection.
-		if (actor.hasCapability(ZombieCapability.UNDEAD)) {
+		if (deadActor.hasCapability(ZombieCapability.UNDEAD)) {
 			return;
 		}
 		deadTurns += 1;
+		System.out.println(deadActor+" dead for "+deadTurns+" turns.");
+		//cannot resurrect if there is an actor standing on the location
+		if(currentLocation.containsAnActor()) {
+			return;
+		}
+		
 		// if deadTurns more than 5, the corpse has a 25 percent chance to
 		// rise up as a corpse
 		if (deadTurns >= 5 && random.nextInt(101) >= 75) {
 			System.out.println(actor_name + " has risen as a zombie!");
 			currentLocation.addActor(new Zombie(actor_name));
 			currentLocation.removeItem(this);
+			
 		}
 		// guaranteed to rise from the dead if the dead turns reach 10
-		else if (deadTurns == 10) {
+		else if (deadTurns >= 10) {
 			System.out.println(actor_name + " has risen as a zombie!");
 			currentLocation.addActor(new Zombie(actor_name));
 			currentLocation.removeItem(this);
 		}
 
+	}
+	
+	@Override
+	public void tick(Location currentLocation,Actor actor) {
+		// if actor is human proceed with resurrection.
+		if (deadActor.hasCapability(ZombieCapability.UNDEAD)) {
+			return;
+		}
+		deadTurns += 1;
+		System.out.println(deadActor+" dead for "+deadTurns+" turns.");
+		ArrayList<Location> locations= new ArrayList<Location>();
+		//check exits around actor carrying the corpse to get valid exits for zombie to rise up
+		for (Exit exit : currentLocation.getExits()) {
+			Location destination = exit.getDestination();
+			if (destination.canActorEnter(actor)) {
+				locations.add(destination);
+			}
+		}
+		Location spawn_point;
+		if (!locations.isEmpty()) {
+			spawn_point=locations.get(random.nextInt(locations.size()));
+		}
+		//quit method if no available location for zombie to spawn
+		else {
+			return;
+		}
+		// if deadTurns more than 5, the corpse has a 25 percent chance to
+		// rise up as a corpse
+		if (deadTurns >= 5 && random.nextInt(101) >= 75) {
+			System.out.println(actor_name + " has risen as a zombie!");
+			spawn_point.addActor(new Zombie(actor_name));
+			actor.removeItemFromInventory(this);;
+					
+		}
+		// guaranteed to rise from the dead if the dead turns reach 10
+		else if (deadTurns == 10) {
+			System.out.println(actor_name + " has risen as a zombie!");
+			spawn_point.addActor(new Zombie(actor_name));
+			actor.removeItemFromInventory(this);;
+		}
+		
 	}
 	
 	@Override
