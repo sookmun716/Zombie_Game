3c592f7b9b18c5eb3869a6647791c3f619f2d2ea
Hew Sook Mun <shew0010@student.monash.edu>
2020-05-20 12:29:25 +0800

    Modified the weapon Zombie can pick up

--- src/game/PickUpBehaviour.java
+++ src/game/PickUpBehaviour.java
@@ -5,14 +5,20 @@ import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Item;
 import edu.monash.fit2099.engine.PickUpItemAction;
+import edu.monash.fit2099.engine.Weapon;
+
+import java.util.ArrayList;
 
 public class PickUpBehaviour implements Behaviour{
-
+	
+	ArrayList<Weapon> pickUpWeapon = new ArrayList<Weapon>();
+	
 	@Override
 	public Action getAction(Actor actor, GameMap map) {
 		if(actor.hasCapability(ZombieCapability.UNDEAD)) {
 			for(Item item : map.locationOf(actor).getItems()) {
-				if(item.getDisplayChar()==')') {
+				if(item.getDisplayChar()==')'||item.getDisplayChar()=='A'||item.getDisplayChar()=='L'
+											||item.getDisplayChar()=='M'||item.getDisplayChar()=='C') {
 					return new PickUpItemAction(item);
 				}
 			}
