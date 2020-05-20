679274f3f940be2a5b303a93bc01e22f7f7cdff3
Tan Song Shun <stan0101@student.monash.edu>
2020-05-20 21:35:49 +0800

    Updated the two tick methods so that crafting is only available when in player inventory

--- src/game/ZombieArm.java
+++ src/game/ZombieArm.java
@@ -1,13 +1,27 @@
 package game;
 
 import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.Location;
 import edu.monash.fit2099.engine.WeaponItem;
 
-public class ZombieArm extends WeaponItem{
-	
+public class ZombieArm extends WeaponItem {
+	CraftWeaponAction crafting_action = new CraftWeaponAction(this);
+
 	public ZombieArm() {
-		super("zombie arm",'A',20,"hits");
-		this.allowableActions.add(new CraftWeaponAction(this));
+		super("zombie arm", 'A', 20, "hits");
+	}
+
+	@Override
+	public void tick(Location currentLocation) {
+		if (this.getAllowableActions().contains(crafting_action)) {
+			// remove crafting action if limb on ground
+			this.allowableActions.remove(crafting_action);
+		}
+	}
+
+	@Override
+	public void tick(Location currentLocation, Actor actor) {
+		this.allowableActions.add(crafting_action);
 	}
 
 	@Override
--- src/game/ZombieLeg.java
+++ src/game/ZombieLeg.java
@@ -1,16 +1,28 @@
 package game;
 
 import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.Location;
 import edu.monash.fit2099.engine.WeaponItem;
 
 public class ZombieLeg extends WeaponItem{
-	
+	CraftWeaponAction crafting_action= new CraftWeaponAction(this);
 	public ZombieLeg() {
 		super("zombie leg",'L',25,"hits");
 		this.allowableActions.add(new CraftWeaponAction(this));
 	}
 
-
+	@Override
+	public void tick(Location currentLocation) {
+		if(this.getAllowableActions().contains(crafting_action)) {
+			//remove crafting action if limb on ground
+			this.allowableActions.remove(crafting_action);
+		}
+	}
+	
+	@Override
+	public void tick(Location currentLocation, Actor actor) {
+		this.allowableActions.add(crafting_action);
+	}
 	@Override
 	public String craft(Actor actor) {
 		actor.addItemToInventory(new ZombieMace());
