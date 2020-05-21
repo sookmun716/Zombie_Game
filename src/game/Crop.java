0c1daf0d01e45d3394befe9b878e065285ae7393
Tan Song Shun <stan0101@student.monash.edu>
2020-05-21 13:34:12 +0800

    Added isRipe instance variable and changed isRipe() and tick() method to ensure that the game does not print the crop condition when it is ripe multiple times

--- src/game/Crop.java
+++ src/game/Crop.java
@@ -12,6 +12,7 @@ import edu.monash.fit2099.engine.Location;
 public class Crop extends Ground{
 	private int ripeTurns=0;
 	private Boolean fertilised=false;
+	private Boolean isRipe=false;
 	/**
 	 * This is the constructor for the Crop class, the super constructor is
 	 * called to instantiate the object.
@@ -34,10 +35,11 @@ public class Crop extends Ground{
 	public void tick(Location location) {
 		ripeTurns+=1;
 		//Change display character if crop is ripe
-		if(ripeTurns>=20){
+		if(ripeTurns>=20 && !isRipe){
 			this.displayChar='!';
 			//set fertilised to true because ripe crops should not be fertilised
 			fertilised=true;
+			isRipe=true;
 			System.out.println("Crop at "+location.x()+','+location.y()+" is ripe.");
 		}
 	}
@@ -56,10 +58,7 @@ public class Crop extends Ground{
 	
 	@Override
 	public Boolean isRipe() {
-		if (ripeTurns>=20) {
-			return true;
-		}
-		return false;
+		return isRipe;
 	}
 
 	@Override
