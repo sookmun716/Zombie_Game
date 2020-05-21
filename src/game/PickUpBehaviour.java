b2ea5eeeafabbf42142c87b1fd9e1ddb15d74748
Hew Sook Mun <shew0010@student.monash.edu>
2020-05-21 17:29:38 +0800

    Added javadoc comments

--- src/game/PickUpBehaviour.java
+++ src/game/PickUpBehaviour.java
@@ -6,8 +6,14 @@ import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Item;
 import edu.monash.fit2099.engine.PickUpItemAction;
 
+/**
+ * A subclass of Behaviour that represents picking up weapons for zombie. 
+ * If there is any weapon at Zombie's location then it will pick it up.
+ * @author SookMun
+ *
+ */
 public class PickUpBehaviour implements Behaviour{
-	
+
 	@Override
 	public Action getAction(Actor actor, GameMap map) {
 		try {
