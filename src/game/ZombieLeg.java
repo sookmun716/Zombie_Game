f363345c494f2d97d3c41f04741292f83c62c7f2
Hew Sook Mun <shew0010@student.monash.edu>
2020-05-21 16:23:24 +0800

    Added javadoc comments

--- src/game/ZombieLeg.java
+++ src/game/ZombieLeg.java
@@ -4,6 +4,15 @@ import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.Location;
 import edu.monash.fit2099.engine.WeaponItem;
 
+/**
+ * A subclass of WeaponItem that represents ZombieLeg
+ * Leg will be dropped by zombie and picked up by Zombie or Player.
+ * It has damage of 25 and verb of "hits".
+ * Dropped leg can be crafted into Zombie Mace by Player after Player picks up.
+ * @author SookMun
+ *
+ */
+
 public class ZombieLeg extends WeaponItem{
 	CraftWeaponAction crafting_action= new CraftWeaponAction(this);
 	public ZombieLeg() {
