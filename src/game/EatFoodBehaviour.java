9cba8a5c7ad00039516ff2d36d47921068be9404
Tan Song Shun <stan0101@student.monash.edu>
2020-05-22 16:24:16 +0800

    Formatted code to make it more readable

--- src/game/EatFoodBehaviour.java
+++ src/game/EatFoodBehaviour.java
@@ -5,39 +5,41 @@ import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Item;
+
 /**
- * A subclass of Behaviour that represents the eating of food, only humans can eat 
- * food and humans will only eat food when they are damaged. 
+ * A subclass of Behaviour that represents the eating of food, only humans can
+ * eat food and humans will only eat food when they are damaged.
  * 
  * @author Tan Song Shun
  *
  */
-public class EatFoodBehaviour implements Behaviour{
+public class EatFoodBehaviour implements Behaviour {
 
 	@Override
 	public Action getAction(Actor actor, GameMap map) {
-		//Undead cannot heal through food
-		if(actor.hasCapability(ZombieCapability.UNDEAD)) return null;
-		
-		//loop through the items on the location to find out whether food is available
-		for(Item item : map.locationOf(actor).getItems()) {
-			//if location contains food humans can eat it
+		// Undead cannot heal through food
+		if (actor.hasCapability(ZombieCapability.UNDEAD))
+			return null;
+
+		// loop through the items on the location to find out whether food is available
+		for (Item item : map.locationOf(actor).getItems()) {
+			// if location contains food humans can eat it
 			try {
-			if(item.isFood()) {
-				//only eat food when human is hurt
-				if(actor.getMaxHp()==actor.getHp()) {
-					return null;
+				if (item.isFood()) {
+					// only eat food when human is hurt
+					if (actor.getMaxHp() == actor.getHp()) {
+						return null;
+					}
+					return new EatAction(item);
 				}
-				return new EatAction(item);
 			}
-			}
-			//ignore exception because item is not food
-			catch(NullPointerException e) {
+			// ignore exception because item is not food
+			catch (NullPointerException e) {
 				continue;
 			}
 		}
 		return null;
-			
+
 	}
 
 }
