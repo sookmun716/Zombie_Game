7f180fbce87cf2683557e6cdffa175e00d9b094a
Tan Song Shun <stan0101@student.monash.edu>
2020-06-05 13:51:34 +0800

    Created shotgun ammunition box class

---
+++ src/game/ShotgunAmmunitionBox.java
@@ -0,0 +1,19 @@
+package game;
+
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.Location;
+
+public class ShotgunAmmunitionBox extends PortableItem{
+
+	public ShotgunAmmunitionBox() {
+		super("Shotgun Ammunition Box", 'b');
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void tick(Location currentLocation, Actor actor) {
+		actor.add_shotgun_ammo(5);
+		actor.removeItemFromInventory(this);
+	}
+
+}
