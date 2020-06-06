1683597b978583384e810e5cdfb9e45b0aa25b06
Tan Song Shun <stan0101@student.monash.edu>
2020-06-06 16:55:48 +0800

    Removed stub comment from constructor

--- src/game/ShotgunAmmunitionBox.java
+++ src/game/ShotgunAmmunitionBox.java
@@ -4,10 +4,8 @@ import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.Location;
 
 public class ShotgunAmmunitionBox extends PortableItem{
-
 	public ShotgunAmmunitionBox() {
 		super("Shotgun Ammunition Box", 'b');
-		// TODO Auto-generated constructor stub
 	}
 	
 	@Override
