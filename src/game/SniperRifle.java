8d4a5eef9149538c389668bdd3b108b623213d76
Tan Song Shun <stan0101@student.monash.edu>
2020-06-06 16:59:27 +0800

    Implemented prototype for SniperRifle class that represents a sniper rifle

---
+++ src/game/SniperRifle.java
@@ -0,0 +1,124 @@
+package game;
+
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Location;
+import edu.monash.fit2099.engine.WeaponItem;
+
+public class SniperRifle extends WeaponItem {
+	private int aim_turns=0;
+	private Actor target;
+	private GameMap map;
+	private int ranged_damage=40;
+	
+	public SniperRifle(GameMap map) {
+		super("Sniper Rifle", 'S', 15, "clubs");
+		this.map=map;
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void tick(Location currentLocation, Actor actor) {
+		try {
+			if(actor.damaged()) {
+				reset();
+				System.out.println(actor+" concentration was broken due to receiving damage!");
+			}
+		}
+		catch(NullPointerException e){
+			return;
+		}
+		
+		try {
+			if(aim_turns>=1&&actor.get_lastAction().isSniperAction());
+		}
+		catch(NullPointerException e){
+			reset();
+			System.out.println(actor+" concentration was broken due to taking an action other than aiming and firing.");
+		}
+		
+		super.allowableActions.clear();
+		for (int y : map.getYRange()) {
+			for (int x : map.getXRange()) {
+				if(map.at(x, y).containsAnActor()) {
+					Actor snipe_target=map.at(x, y).getActor();
+					if(snipe_target.hasCapability(ZombieCapability.UNDEAD)) {
+						super.allowableActions.add(new AimAction(snipe_target,this));
+						super.allowableActions.add(new SnipeAction(snipe_target, this));
+					}
+					
+				}
+			}
+		}
+	}
+	
+	@Override
+	public void tick(Location currentLocation) {
+		super.allowableActions.clear();
+		//broken concentration if sniper is dropped. 
+		reset();
+	}
+	@Override
+	public String craft(Actor actor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String craftDescription(Actor actor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean isFood() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean isWeapon() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public void changeMap(GameMap map) {
+		this.map=map;
+		
+	}
+	
+	@Override
+	public void setTarget(Actor actor) {
+		target=actor;
+	}
+	
+	@Override
+	public Actor getTarget() {
+		// TODO Auto-generated method stub
+		return target;
+	}
+	
+	@Override
+	public void set_aim_turns(int count) {
+		aim_turns=count;
+	}
+	
+	@Override
+	public int get_aim_turns() {
+		return aim_turns;
+	}
+	
+	@Override
+	public void reset() {
+		aim_turns=0;
+		target=null;
+	}
+
+	@Override
+	public int getRangedDamage() {
+		// TODO Auto-generated method stub
+		return ranged_damage;
+	}
+
+}
