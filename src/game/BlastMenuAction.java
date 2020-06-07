51411fbb9493473b2252f953dd85a7658db2eab1
Tan Song Shun <stan0101@student.monash.edu>
2020-06-07 15:12:17 +0800

    Implemented BlastMenuAction which will display a submenu that allows player to choose the direction of shotgun fire

---
+++ src/game/BlastMenuAction.java
@@ -0,0 +1,45 @@
+package game;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actions;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.Display;
+import edu.monash.fit2099.engine.Exit;
+import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Location;
+import edu.monash.fit2099.engine.Menu;
+import edu.monash.fit2099.engine.WeaponItem;
+
+public class BlastMenuAction extends Action {
+	private Menu menu=new Menu();
+	private Display display=new Display();
+	private Actions actions=new Actions();
+	private WeaponItem shotgun;
+	
+	public BlastMenuAction(WeaponItem weapon) {
+		shotgun=weapon;
+	}
+	
+	@Override
+	public Boolean isSniperAction() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String execute(Actor actor, GameMap map) {
+		Location actor_location=map.locationOf(actor);
+		for (Exit exit : actor_location.getExits()) {
+			actions.add(new BlastAction(shotgun, exit));
+		}
+		Action action=menu.showMenu(actor, actions, display);
+		return action.execute(actor,map);
+	}
+
+	@Override
+	public String menuDescription(Actor actor) {
+		// TODO Auto-generated method stub
+		return actor+" fires shotgun";
+	}
+
+}
