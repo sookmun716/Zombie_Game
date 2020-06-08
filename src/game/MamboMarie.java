3f993bed48bb595ed88e21a77bca464874d031ff
Tan Song Shun <stan0101@student.monash.edu>
2020-06-08 15:35:23 +0800

    Added CHANT_ROUND and chant_round()
    CHANT_ROUND represents the number of turns between each chanting and
    chant_round() is its accessor

--- src/game/MamboMarie.java
+++ src/game/MamboMarie.java
@@ -13,7 +13,7 @@ public class MamboMarie extends ZombieActor{
 		new WanderBehaviour()
 		};
 	private int turn_count=0;
-	
+	public int CHANT_ROUND=10;
 	public MamboMarie() {
 		super("Mambo Marie", 'M', 200, ZombieCapability.UNDEAD);
 		// TODO Auto-generated constructor stub
@@ -135,6 +135,12 @@ public class MamboMarie extends ZombieActor{
 		}
 		 return "";
 			
+	}
+
+	@Override
+	public int chant_round() {
+		// TODO Auto-generated method stub
+		return CHANT_ROUND;
 	}
 
 }
