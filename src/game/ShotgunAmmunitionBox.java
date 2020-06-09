a03092f48364a95b6c287bed48b156f0bc60cb7f
Tan Song Shun <stan0101@student.monash.edu>
2020-06-09 09:42:12 +0800

    Changed Magic Number with an instance variable for ammunition count

--- src/game/ShotgunAmmunitionBox.java
+++ src/game/ShotgunAmmunitionBox.java
@@ -4,13 +4,14 @@ import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.Location;
 
 public class ShotgunAmmunitionBox extends PortableItem{
+	private int ammo_count=5;
 	public ShotgunAmmunitionBox() {
 		super("Shotgun Ammunition Box", 'b');
 	}
 	
 	@Override
 	public void tick(Location currentLocation, Actor actor) {
-		int count=actor.get_shotgun_ammo()+5;
+		int count=actor.get_shotgun_ammo()+ammo_count;
 		actor.set_shotgun_ammo(count);
 		actor.removeItemFromInventory(this);
 	}
--- src/game/SniperAmmunitionBox.java
+++ src/game/SniperAmmunitionBox.java
@@ -4,14 +4,14 @@ import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.Location;
 
 public class SniperAmmunitionBox extends PortableItem{
-
+	private int ammo_count=5;
 	public SniperAmmunitionBox() {
 		super("Sniper Ammunition Box", 'B');
 	}
 	
 	@Override
 	public void tick(Location currentLocation, Actor actor) {
-		int count=actor.get_sniper_ammo()+5;
+		int count=actor.get_sniper_ammo()+ammo_count;
 		actor.set_sniper_ammo(count);
 		actor.removeItemFromInventory(this);
 	}
