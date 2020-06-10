54c91438a590852af0d1a71475a0384a72987de5
Tan Song Shun <stan0101@student.monash.edu>
2020-06-10 19:39:58 +0800

    Removed try catch clause as it serves no real purpose

--- src/game/SnipeAction.java
+++ src/game/SnipeAction.java
@@ -21,23 +21,16 @@ public class SnipeAction extends Action{
 	}
 	@Override
 	public String execute(Actor actor, GameMap map) {
-		Display display = new Display();
 		sniper.use_ammo(actor);
 		String result=null;
 		if(actor.get_sniper_ammo()==0) {
 			return actor+" has no sufficient sniper ammunition, sniper cannot be fired";
 		}
-		
 		if(sniper.aim_turns()==0) {
 			if(random.nextInt(101)>=25) {
 				target.hurt(sniper.ranged_damage());
-				try {
-					result=actor+" "+sniper.secondary_verb()+" "+target+ " for "+sniper.ranged_damage()+" damage";
-				}
-				catch(NullPointerException e){
-					display.println(sniper+" is not a sniper rifle.");
-					System.exit(0);
-				}
+				result=actor+" "+sniper.secondary_verb()+" "+target+ " for "+sniper.ranged_damage()+" damage";
+				
 			}
 			else {
 				result=actor+" misses "+target+"!";
@@ -48,13 +41,7 @@ public class SnipeAction extends Action{
 			if(random.nextInt(101)>=10) {
 				int double_damage=sniper.ranged_damage()*2;
 				target.hurt(double_damage);
-				try {
-					result=actor+" "+sniper.secondary_verb()+" "+target+ " for "+double_damage+" damage";
-				}
-				catch(NullPointerException e){
-					display.println(sniper+" is not a sniper rifle.");
-					System.exit(0);
-				}
+				result=actor+" "+sniper.secondary_verb()+" "+target+ " for "+double_damage+" damage";
 			}
 			else {
 				result=actor+" misses "+target+"!";
@@ -63,15 +50,10 @@ public class SnipeAction extends Action{
 		else if(sniper.aim_turns()>=MAXIMUM_FOCUS) {
 			int max_damage=target.getHp();
 			target.hurt(max_damage);
-			try {
-				result=actor+" "+sniper.secondary_verb()+" "+target+ " for "+max_damage+" damage";
-			}
-			catch(NullPointerException e){
-				display.println(sniper+" is not a sniper rifle.");
-				System.exit(0);
-			}
+			result=actor+" "+sniper.secondary_verb()+" "+target+ " for "+max_damage+" damage";
+			
 		}
-		
+
 		result+=target.isDead(map);
 		return result;
 	}
