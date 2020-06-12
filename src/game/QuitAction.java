e4a7461c288428ef1bab99da33b44d83f2c24ef1
Hew Sook Mun <shew0010@student.monash.edu>
2020-06-13 01:39:06 +0800

    Added javadoc comments

--- src/game/QuitAction.java
+++ src/game/QuitAction.java
@@ -6,6 +6,15 @@ import edu.monash.fit2099.engine.GameMap;
 
 public class QuitAction extends Action {
 	
+	/**
+	 * An action where player can choose in the menu. 
+	 * Once the action is chose, it will end the game by removing player from the map.
+	 * A description of player quit the game will be displayed.
+	 * @param actor: the actor performing this action
+	 * @param map: the map the actor is on.
+	 * @return a description of what happened that can be displayed to the user.
+	 * 
+	 */
 	@Override
 	public String execute(Actor actor, GameMap map) {
 		if(Player.class.isInstance(actor)) {
@@ -15,6 +24,11 @@ public class QuitAction extends Action {
 		return null;
 	}
 	
+	/**
+	 * Return a descriptive string when player quits the game. 
+	 * @param actor: the actor performing this action
+	 * @return the text we put on the menu
+	 */
 	@Override
 	public String menuDescription(Actor actor) {
 		return actor + " quit the game!";
